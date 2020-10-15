dd1094dbacf76c29502121a355d4675f7f3f40bc
Demi-O <demiladeolagoke@yahoo.ca>
2020-10-14 19:30:30 -0700

    no idea where I left off but saving progress

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,54 @@
+// APP_ID and APP_KEY are defined in my private.js file
+// This file is not included in this repo as these are credentials unique to my account
+// at https://developer.oxforddictionaries.com/
 
+// Constants
+var BASE_URL = 'https://od-api.oxforddictionaries.com/api/v1/';
+var ENTRIES_BASE = 'entries/'
+var SOURCE_LANG = 'en/';
+
+// Globals
+var getMeaning = true;
+var getSynonym;
+var getAntonym;
+var getExample;
+
+function findButtonHandler() {
+  var word = document.getElementById('word_input').value; 
+  var url;
+
+  // Make API requests based on what user wants
+  if (getMeaning) {
+    var req = new XMLHttpRequest();
+    url = BASE_URL + ENTRIES_BASE + SOURCE_LANG + word + '/definitions';
+    req.open("GET", url, true);
+    // Set Header
+    // req.setRequestHeader('Access-Control-Allow-Origin', '*');
+    req.setRequestHeader('Accept', 'application/json');
+    req.setRequestHeader('app_id', APP_ID);
+    req.setRequestHeader('app_key', APP_KEY);
+
+    console.log('in meaning');
+    req.onreadystatechange = function() {
+      if (req.readyState == 4 && req.status == 200) {
+        console.log('hii');
+        console.log(req.responseText);
+      }
+      else{
+        console.log('bye');
+        console.log(req.readyState);
+        console.log(req.status);
+      }
+    };
+    req.send(null);
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // setGlobalVariables();
+
+  // Add Event Listeners
+  document.getElementById("word_button").addEventListener("click", findButtonHandler);
+  // chrome.extension.getBackgroundPage().console.log('foo');
+}); 
---
+++ js/private.js
@@ -0,0 +1,2 @@
+var APP_ID = 'e1c01ace';
+var APP_KEY = '8f5d698ca6486711f6be2dec2bb7a8ea'; 
